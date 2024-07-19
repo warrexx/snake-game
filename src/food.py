2256843215b0c37f50f2eb772f58a927e7990c7b
Firhand Hawarie <55737624+warrexx@users.noreply.github.com>
2024-07-20 00:14:40 +0900

    first commit added food class - completed added snake class - on going

---
+++ src/food.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+from random import randint
+
+FOOD_COLOR = "red"
+FOOD_SHAPE = "circle"
+FOOD_SIZE = {"len": 0.5, "wid": 0.5}
+
+
+class Food(Turtle):
+    def __init__(self, height: float, width: float):
+
+        super().__init__()
+        self.color(FOOD_COLOR)
+        self.shape(FOOD_SHAPE)
+        self.shapesize(FOOD_SIZE["len"], FOOD_SIZE["wid"])
+        self.penup()
+
+        self.x_range = int(height / 2 - 20)
+        self.y_range = int(width / 2 - 20)
+
+        x, y = randint(self.x_range, self.x_range), randint(-self.y_range, self.y_range)
+        self.goto(x, y)
+
+    def eaten(self) -> None:
+        x, y = randint(self.x_range, self.x_range), randint(-self.y_range, self.y_range)
+        self.goto(x, y)
---
+++ src/snake.py
@@ -0,0 +1,7 @@
+from turtle import Turtle
+from random import randint
+
+STARTER_LENGTH = 3
+RIGHT, UP, LEFT, DOWN = 0, 90, 180, 270
+COLOR = "white"
+SHAPE = "square"
